2780c4002b48b4f79f397bcdf42c2c12a7266789
Nathan Tolley <nathantolley2008@gmail.com>
2026-09-16 19:11:33 -0400

    Update register api endpoint

--- api/auth/register.php
+++ api/auth/register.php
@@ -12,5 +12,34 @@ $db     = getDB();
 if ($method !== 'POST') {
     respond(405, ['error' => 'Method not allowed']);
 }
-    
-respond(200, ['error' => 'TODO']);
+
+$body = getRequestBody();
+
+if (isset($body['username']) && isset($body['password'])) {
+    $username    = clean($body['username']);
+    $password = $body['password'];
+    $firstName = clean($body['first_name'] ?? '');
+    $lastName = clean($body['last_name'] ?? '');
+    $userRole = clean($body['user_role'] ?? 'user');
+
+    if (!$username || !$password) {
+        respond(400, ['error' => 'Username and password are required']);
+    }
+
+    // Check if username exists already
+    $stmt = $db->prepare('SELECT 1 FROM Users WHERE Login = :username LIMIT 1');
+    $stmt->execute([':username' => $username]);
+    $user = $stmt->fetch();
+
+    if($user){
+        respond(400, ['error' => 'User already exists with that username']);
+    }
+
+    $dateCreated = date('Y-m-d H:i:s');
+    $dateUpdated = date('Y-m-d H:i:s');
+
+    $stmt = $db->prepare('INSERT INTO Users (FirstName, LastName, Login, Password, DateCreated, DateUpdated, UserRole) VALUES (:firstname, :lastname, :login, :password, :datecreated, :dateupdated, :userrole)');
+    $stmt->execute([':firstname' => $firstName, ':lastname' => $lastName, ':login' => $username, ':password' => $password, ':datecreated' => $dateCreated, ':dateupdated' => $dateUpdated, ':userrole' => $userRole]);
+
+    respond(201, ['message' => 'User created successfully']);
+}
